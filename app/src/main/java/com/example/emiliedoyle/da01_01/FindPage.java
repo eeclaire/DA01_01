030a1558a0be29e706e23696d50a4d8c90a8e025
Claire <claire.durand@temple.edu>
2015-10-05 20:34:39 -0400

    Initial commit (DB functionality not functioning yet)

--- app/src/main/java/com/example/emiliedoyle/da01_01/FindPage.java
+++ app/src/main/java/com/example/emiliedoyle/da01_01/FindPage.java
@@ -3,33 +3,121 @@ package com.example.emiliedoyle.da01_01;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Context;
 import android.widget.CheckBox;
+import android.widget.TextView;
+
+// Volley imports
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindPage extends AppCompatActivity {
+
+    // Instantiate the RequestQueue.
+    private RequestQueue queue;
+    private String url;
+    String major;
+
+
+    public void getDBItems(){
+        // This is from the Volley tutorial
+        final TextView mTextView = (TextView) findViewById(R.id.db_contents);
+
+        // Request a string response from the provided URL.
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the response string (items of the DB)
+                        mTextView.setText("Database contents: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("Well fuck.");
+            }
+        });
+        // End of tutorial from Volley
+
+        // Add the request to the RequestQueue.
+        queue.add(getRequest);
+
+    }
+
+
+    public void postMajor(){
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //getDBItems();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+
+            }
+        }){
+            @Override
+        protected Map<String,String> getParams(){
+
+                Log.d("POSTING", "The mapping thing is happening");
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("major", major);
+
+                return params;
+            }
+        };
+
+        queue.add(postRequest);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_page);
+
+        // Define queue and url
+        queue = Volley.newRequestQueue(this);
+        url ="http://159.203.68.208:5000/data";
+
         final Context context = this;
         Button search_results_button = (Button) findViewById(R.id.SearchResultsButton);
         search_results_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Try to post the major to the DB
+                Log.d("LISTENER", "Stuff can happen here.");
+                postMajor();
+
                 Intent search_results_screen = new Intent(context,SearchResultsPage.class);
                 startActivity(search_results_screen);
             }
         });
+
+        // Try it here?
+        getDBItems();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_find_page, menu);
+
         return true;
     }
 
@@ -48,11 +136,11 @@ public class FindPage extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean EE_flag;
+    /*public boolean EE_flag;
     public boolean ME_flag;
     public boolean BE_flag;
     public boolean CE_flag;
-    public boolean CM_flag;
+    public boolean CM_flag; */
 
     public void onCheckboxClicked(View view) {
         // Is the view now checked?
@@ -62,33 +150,43 @@ public class FindPage extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.EE_box:
                 if (checked)
-                    EE_flag = true;
+                    major = "Electrical Engineering";
+                    //EE_flag = true;
                 else
-                    EE_flag = false;
+                    major = "unknown";
+                    //EE_flag = false;
                 break;
             case R.id.ME_box:
                 if (checked)
-                    ME_flag = true;
+                    major = "Mechanical Engineering";
+                    //ME_flag = true;
                 else
-                    ME_flag = false;
+                    major = "unknown";
+                    //ME_flag = false;
                 break;
             case R.id.CE_box:
                 if (checked)
-                    CE_flag = true;
+                    major = "Civil Engineering";
+                    //CE_flag = true;
                 else
-                    CE_flag = false;
+                    major = "unknown";
+                    //CE_flag = false;
                 break;
             case R.id.BE_box:
                 if (checked)
-                    BE_flag = true;
+                    major = "Bioengineering";
+                    //BE_flag = true;
                 else
-                    BE_flag = false;
+                    major = "unknown";
+                    //BE_flag = false;
                 break;
             case R.id.CM_box:
                 if (checked)
-                    CM_flag = true;
+                    major = "Construction Management";
+                    //CM_flag = true;
                 else
-                    CM_flag = false;
+                    major = "unknown";
+                    //CM_flag = false;
                 break;
         }
     }
--- app/src/main/java/com/example/emiliedoyle/da01_01/SearchResultsPage.java
+++ app/src/main/java/com/example/emiliedoyle/da01_01/SearchResultsPage.java
@@ -4,13 +4,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class SearchResultsPage extends AppCompatActivity {
+
+    // Instantiate the RequestQueue.
+    private RequestQueue queue;
+    private String url;
+    String major;
+
+    public void getMajor(){
+        final TextView mTextView = (TextView) findViewById(R.id.search_result);
+
+        // Request a string response from the provided URL.
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the response string (items of the DB)
+                        mTextView.setText("Your major is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(getRequest);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results_page);
+
+        // Define queue and url
+        queue = Volley.newRequestQueue(this);
+        url ="http://159.203.68.208:5000/data";
+
+        getMajor();
     }
 
     @Override
